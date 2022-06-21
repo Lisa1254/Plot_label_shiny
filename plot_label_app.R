fa2e910aa4273c44ea4e8521308597742a0302c9
Lisa Hoeg <lhoeg@durocherimac3.local>
2022-06-21 16:13:05 -0400

    allows up to 3 highlight colours

--- plot_label_app.R
+++ plot_label_app.R
@@ -1,15 +1,24 @@
 ## Libraries ---- 
 
 library(shiny)
+library(purrr)
 library(ggplot2)
 library(ggrepel)
 
 col_hex <- setNames(c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7", "lightgray"), 
                     c("Black", "Orange", "Sky Blue", "Bluish Green", "Yellow", "Blue", "Red", "Purple", "Light Gray"))
 
+condPan01 <- function(number) {
+  conditionalPanel(paste0("input.num_gps >=", as.numeric(number)),
+                   fluidRow(column(3, textInput(paste0("gp", number), label = paste0("Group Name: "))),
+                            column(3, selectInput(paste0("col", number), label = paste0("Colour for Group ", number),
+                                                  choices = col_hex))))
+}
+map_conds <- map(c(1,2,3), condPan01)
 
 ## UI ----
 ui <- fluidPage(
+  titlePanel("Scatterplot with Custom Labels"),
   fluidRow(column(5, fileInput("txt_data", "Supply tab delimited .txt file", width = "100%")),
            column(3, checkboxInput("preview", "Show preview of data"))),
   conditionalPanel(
@@ -29,8 +38,13 @@ ui <- fluidPage(
                             column(3, selectInput("yp", "Positive Score (Y values)", choices = NULL)))),
 
   fluidRow(column(4, selectInput("col_m", "Main colour", choices = col_hex)),
-           column(4, selectInput("col_a", "Accent colour", choices = col_hex)),
            column(4,checkboxGroupInput("y_trans", "Transformations for y-axis:", c("log10", "reverse")))),
+  
+  sliderInput("num_gps", "How many accent groups?", min=0, max=3, step=1, value=0),
+  map_conds,
+  conditionalPanel("input.num_gps >=1",
+                   radioButtons("current_gp", "Current group for labelling", choices = "none")),
+  
   actionButton("plot", "Generate/Reset Scatterplot"),
 
   fluidRow(column(9, plotOutput("scatter", click = "plot_click", hover = "plot_hover", brush = "plot_brush")),
@@ -91,6 +105,22 @@ server <- function(input, output, session) {
     }
   })
   
+  all_labels <- reactive(c(input$gp1, input$gp2, input$gp3)[1:input$num_gps])
+  
+  #Need to figure out how to better iterate or make a module/function for this
+  observeEvent(input$gp1, {
+    updateSelectInput(inputId = "col1", label = paste0("Colour for ", input$gp1))
+    updateRadioButtons(inputId = "current_gp", choices =all_labels())
+  })
+  observeEvent(input$gp2,{
+    updateSelectInput(inputId = "col2", label = paste0("Colour for ", input$gp2))
+    updateRadioButtons(inputId = "current_gp", choices =all_labels())
+  })
+  observeEvent(input$gp3,{
+    updateSelectInput(inputId = "col3", label = paste0("Colour for ", input$gp3))
+    updateRadioButtons(inputId = "current_gp", choices =all_labels())
+  })
+  
   plot_LFC_data <- reactive({
     req(input$plot)
     if ("log10" %in% input$y_trans){
@@ -110,19 +140,22 @@ server <- function(input, output, session) {
   })
   
   cols <- reactive({
-    c("Main" = input$col_m, "Gp1" = input$col_a)
+    c("Main" = input$col_m, "Gp1" = input$col1, "Gp2" = input$col2, "Gp3" = input$col3)[1:(input$num_gps+1)]
   })
+  
   
   output$scatter <- renderPlot({
     input$plot
     plot_gp_data <- plot_LFC_data()
-    plot_gp_data$Col <- gp_data()
-    plot_gp_data$Col <- ifelse(plot_gp_data$ID %in% selected(), "Gp1", plot_gp_data$Col)
+    plot_gp_data$Gp <- gp_data()
+    plot_gp_data$Gp <- ifelse(plot_gp_data$ID %in% selected1(), "Gp1", plot_gp_data$Gp)
+    plot_gp_data$Gp <- ifelse(plot_gp_data$ID %in% selected2(), "Gp2", plot_gp_data$Gp)
+    plot_gp_data$Gp <- ifelse(plot_gp_data$ID %in% selected3(), "Gp3", plot_gp_data$Gp)
     if ("reverse" %in% input$y_trans) {
       ggplot(data=plot_gp_data) +
-        geom_point(aes(x=LFC, y=Score, color = factor(Col)), shape = 16, size = 3) +
+        geom_point(aes(x=LFC, y=Score, color = factor(Gp)), shape = 16, size = 3) +
         scale_color_manual(values = cols()) +
-        geom_text_repel(aes(x=LFC, y=Score, label=ifelse(Col=="Gp1", ID, '')), 
+        geom_text_repel(aes(x=LFC, y=Score, label=ifelse(Gp=="Main", '', ID)), 
                         min.segment.length = 0, size = 3, max.overlaps = 15) +
         theme(legend.position = "none", 
                  panel.background = element_rect(fill = "white"), 
@@ -131,9 +164,9 @@ server <- function(input, output, session) {
         scale_y_continuous(trans = "reverse")
     } else {
       ggplot(data=plot_gp_data) +
-        geom_point(aes(x=LFC, y=Score, color = factor(Col)), shape = 16, size = 3) +
+        geom_point(aes(x=LFC, y=Score, color = factor(Gp)), shape = 16, size = 3) +
         scale_color_manual(values = cols()) +
-        geom_text_repel(aes(x=LFC, y=Score, label=ifelse(Col=="Gp1", ID, '')), 
+        geom_text_repel(aes(x=LFC, y=Score, label=ifelse(Gp=="Main", '', ID)), 
                         min.segment.length = 0, size = 3, max.overlaps = 15) +
         theme(legend.position = "none", 
                  panel.background = element_rect(fill = "white"), 
@@ -158,26 +191,44 @@ server <- function(input, output, session) {
     nearPoints(plot_LFC_data(), input$plot_click)[,1]
   })
   
-  selected <- reactiveVal({
+  selected1 <- reactiveVal({
+    vector()
+  })
+  selected2 <- reactiveVal({
+    vector()
+  })
+  selected3 <- reactiveVal({
     vector()
   })
   
-  observeEvent(input$plot_click,
-               selected(c(id_lab(), selected()))  
-  )
+  observeEvent(input$plot_click, {
+    gp_sel <- paste0("gp", as.character(which(all_labels() == input$current_gp)))
+    switch(gp_sel,
+           gp1 = selected1(c(id_lab(), selected1())),
+           gp2 = selected2(c(id_lab(), selected2())),
+           gp3 = selected3(c(id_lab(), selected3()))
+    )
+  })
   
 #Add brushed points to selected
   brush_sel <- reactive({
     brushedPoints(plot_LFC_data(), input$plot_brush)[,1]
   })
-  observeEvent(input$plot_brush,
-               selected(c(brush_sel(), selected())))
-
+  observeEvent(input$plot_brush,{
+    gp_sel <- paste0("gp", as.character(which(all_labels() == input$current_gp)))
+    switch(gp_sel,
+           gp1 = selected1(c(brush_sel(), selected1())),
+           gp2 = selected2(c(brush_sel(), selected2())),
+           gp3 = selected3(c(brush_sel(), selected3()))
+    )
+  })
   
   #If new plot generated, reset selected info
-  observeEvent(input$plot,
-               selected(vector()))
-
+  observeEvent(input$plot, {
+    selected1(vector())
+    selected2(vector())
+    selected3(vector())
+  })
   
   
 }
