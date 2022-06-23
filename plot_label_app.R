9fdbf5507d53acb84e4ca8a9e13cb37ffdf855eb
Lisa Hoeg <lhoeg@durocherimac3.local>
2022-06-23 11:04:54 -0400

    fixed rownaming issue for subsetting highlight genes by specified range

--- plot_label_app.R
+++ plot_label_app.R
@@ -52,15 +52,13 @@ list_split <- function(in_var){
 }
 
 #Function to return genes within desired range
-#To fix this to include rownames as option, just remove colN altogether from this function. Update the use downstream to reflect type of name input and extract the column or rownames from there.
-#Also, for the input of selecting where the names are coming from, use the list format within the choices, so that it is clear that the options are rownames of the dataframe, or one of these colnames.
-#While I'm at it updating that, should probably update the x/y placeholders from Choose One to something like Select a Dataframe Column to be more explicit about where the information is coming from.
-subset_genes <- function(df, colx, minx, maxx, all_y, miny, maxy, colN) {
-  df2 <- df[which((all_y >= miny) & (all_y <= maxy)),]
-  df2 <- df2[which((df2[,colx] >= minx) & (df2[,colx] <= maxx)),]
-  genes_sub <- as.vector(df2[,colN])
+subset_genes <- function(df, colx, minx, maxx, all_y, miny, maxy, vecN){
+  genes_ind <- which((all_y >= miny) & (all_y <= maxy) 
+                     & (df[,colx] >= minx) & (df[,colx] <= maxx))
+  genes_sub <- vecN[genes_ind]
   return(genes_sub)
 }
+
 
 ## UI ----
 ui <- fluidPage(
@@ -232,7 +230,7 @@ server <- function(input, output, session) {
  
   gene_sub1 <- reactive({
     if (input$type1 == "Specified Values") {
-      subset_genes(data(), input$x, input$minX1, input$maxX1, y_vals(), input$minY1, input$maxY1, input$name)
+      subset_genes(data(), input$x, input$minX1, input$maxX1, y_vals(), input$minY1, input$maxY1, data_names())
     } else {
       vector()
     }
@@ -240,7 +238,7 @@ server <- function(input, output, session) {
   
   gene_sub2 <- reactive({
     if (input$type2 == "Specified Values") {
-      subset_genes(data(), input$x, input$minX2, input$maxX2, y_vals(), input$minY2, input$maxY2, input$name)
+      subset_genes(data(), input$x, input$minX2, input$maxX2, y_vals(), input$minY2, input$maxY2, data_names())
     } else {
       vector()
     }
@@ -248,7 +246,7 @@ server <- function(input, output, session) {
   
   gene_sub3 <- reactive({
     if (input$type3 == "Specified Values") {
-      subset_genes(data(), input$x, input$minX3, input$maxX3, y_vals(), input$minY3, input$maxY3, input$name)
+      subset_genes(data(), input$x, input$minX3, input$maxX3, y_vals(), input$minY3, input$maxY3, data_names())
     } else {
       vector()
     }
