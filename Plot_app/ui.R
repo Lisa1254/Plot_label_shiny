b05d310c5d6711b065d0eb503cb15a3a3e342cbb
Lisa Hoeg <lhoeg@durocherimac3.local>
2022-06-24 15:32:08 -0400

    ui: updated styling of ui env: separated environment script, switched gp range values to text input in order to remove sensitive mouse scrolling

--- Plot_app/ui.R
+++ Plot_app/ui.R
@@ -1,71 +1,4 @@
-## Libraries and Environment ---- 
-
-library(shiny)
-library(shinyBS)
-library(purrr)
-library(ggplot2)
-library(ggrepel)
-
-# Colour Choices and corresponding hex or R names
-col_hex <- setNames(c("lightgray", "#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7"), 
-                    c("Light Gray", "Black", "Orange", "Sky Blue", "Bluish Green", "Yellow", "Blue", "Red", "Purple"))
-
-#Reusable conditional for Highlight Groups
-condPan01 <- function(number) {
-  conditionalPanel(paste0("input.num_gps >=", as.numeric(number)),
-                   bsCollapse(id = paste0("collapse_gp", number),
-                              bsCollapsePanel(paste0("Attributes for Highlight Group ", number),
-                                              textInput(paste0("gp", number), label = paste0("Group Name: ")),
-                                              selectInput(paste0("col", number), label = paste0("Colour for Group ", number),
-                                                          choices = col_hex),
-                                              radioButtons(paste0("type", number), label = paste0("Input Type for Group ", number), choices = c("Plot Interaction", "Specified Values", "Gene Input")),
-                                              conditionalPanel(paste0("input.type", number, " == `Specified Values`"),
-                                                               numericInput(paste0("minX", number), "X Value Minumum", value = 0),
-                                                               numericInput(paste0("maxX", number), "X Value Maximum", value = 0),
-                                                               numericInput(paste0("minY", number), "Y Value Minumum", value = 0),
-                                                               numericInput(paste0("maxY", number), "Y Value Maximum", value = 0)
-                                                               
-                                              ), #conditional panel for specified vales end
-                                              conditionalPanel(paste0("input.type", number, " == `Gene Input`"),
-                                                               textAreaInput(paste0("genes", number), "Genes list:", "", width="200px", height="240px")
-                                              ), #Conditional panel  for gene input end
-                                              style = "info"
-                                              ) #End collapse panel
-                              ) #End bsCollapse
-                   
-                   
-                   
-                  
-  ) #end for group's conditional panel
-}
-
-#Apply conditional panel for 3 possible group inputs
-map_conds <- map(c(1,2,3), condPan01)
-
-#Function to parse gene list input 
-list_split <- function(in_var){
-  if(in_var == "") {return(NULL)}
-  
-  s <- unlist(strsplit(
-    unlist(strsplit(
-      unlist(strsplit(
-        unlist(strsplit(toupper(in_var), 
-                        c("[\n]"))),
-        c("[,]"))),
-      c("[:space:]"))),
-    c("\\s")))
-  s <- s[s != ""]
-  
-  return(s)
-}
-
-#Function to return genes within desired range
-subset_genes <- function(df, colx, minx, maxx, all_y, miny, maxy, vecN){
-  genes_ind <- which((all_y >= miny) & (all_y <= maxy) 
-                     & (df[,colx] >= minx) & (df[,colx] <= maxx))
-  genes_sub <- vecN[genes_ind]
-  return(genes_sub)
-}
+source("env.R")
 
 ## UI ----
 ui <- fluidPage(
@@ -112,8 +45,9 @@ ui <- fluidPage(
                                  conditionalPanel(condition = "input.num_y == 1",
                                                   selectInput("y1", "Y values", choices = NULL)),
                                  conditionalPanel(condition = "input.num_y == 2",
-                                                  column(6, selectInput("yn", "Negative Score", choices = NULL)),
-                                                  column(6, selectInput("yp", "Positive Score", choices = NULL))),
+                                                  selectInput("yn", "Negative Score", choices = NULL),
+                                                  selectInput("yp", "Positive Score", choices = NULL)),
+                                                  
                                  textInput("ylab", "Attribute name for y values", placeholder = "e.g. Score (log10)"),
                                  checkboxGroupInput("y_trans", "Transformations for y-axis:", c("log10", "reverse")),
                                  style = "success"
@@ -140,8 +74,9 @@ ui <- fluidPage(
       conditionalPanel(condition = "input.summary == true", verbatimTextOutput("summary_data")),
       
       #Click to plot or reset plot
-      actionButton("plot", "Generate/Reset Scatterplot"),
-      conditionalPanel("input.num_gps >=1",
+      actionButton("plot", "Construct Plot", style = "color: #fff; background-color: #337ab7; border-color: #2e6da4"),
+      conditionalPanel("input.num_gps >=1 & 
+                       (input.type1 == `Plot Interaction` | input.type2 == `Plot Interaction` | input.type3 == `Plot Interaction`)",
                        radioButtons("current_gp", "Current group for labelling", 
                                     choices = "none", inline = T)),
       fluidRow(column(9, plotOutput("scatter", click = "plot_click", hover = "plot_hover", brush = "plot_brush")),
@@ -152,10 +87,9 @@ ui <- fluidPage(
       
       #Download
       fluidRow(column(4, downloadButton("dl_plot", "Save plot as pdf")),
-               column(4, actionButton("dl_genes", "Save selected genes"))),
-               #column(4, downloadButton("dl_genes", "Save selected genes"))),
-      tags$br(),
-      verbatimTextOutput("test_df")
+               column(4, downloadButton("dl_genes", "Save selected genes"))),
+      tags$br()
+      
     ) #end MainPanel
   ) #end sidebarLayout
   
